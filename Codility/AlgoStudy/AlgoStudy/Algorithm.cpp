fa4775f0761a9b0e2d0f176666dd4d4e1f77064d
khs300kr <khs300kr@naver.com>
2022-04-28 04:15:05 +0900

    ArrayQueue 구현

--- Codility/AlgoStudy/AlgoStudy/Algorithm.cpp
+++ Codility/AlgoStudy/AlgoStudy/Algorithm.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <list>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -248,7 +249,6 @@ private:
 	Node<T>* _tail;
 	int		 _size;
 };
-
 template<typename T>
 class Stack
 {
@@ -276,20 +276,81 @@ private:
 	vector<T> _container;
 };
 
+template<typename T>
+class ArrayQueue
+{
+public:
+	ArrayQueue()
+	{
+		_container.resize(100);
+	}
+
+	void push(const T& value)
+	{
+		// TODO : 다 찼는지 체크
+		if (_size == _container.size())
+		{
+			// 증설 작업
+			int newSize = max(1, _size * 2);
+			vector<T> newData;
+			newData.resize(newSize);
+
+			// 데이터 복사
+			for (int i = 0; i < _size; ++i)
+			{
+				int index = (_front + i) % _container.size();
+				newData[i] = _container[index];
+			}
+			_container.swap(newData);
+			_front = 0;
+			_back = _size;
+		}
+
+		_container[_back] = value;
+		_back = (_back + 1) % _container.size();
+		_size++;
+	}
+
+	void pop()
+	{
+		_front = (_front + 1) % _container.size();
+		_size--;
+	}
+
+	T& front()
+	{
+		return _container[_front];
+	}
+
+	bool empty() { return _size == 0; }
+	int size() { return _size; }
+
+private:
+	vector<T> _container;
+
+	int _front = 0;
+	int _back = 0;
+	int _size = 0;
+};
 
 
 void main()
 {
-	stack<int> s;
-	
-	// 삽입
-	s.push(1);
-	s.push(2);
-	s.push(3);
+	ArrayQueue<int> q;
 
-	// 최상위 원소
-	int data = s.top();
+	for (int i = 0; i < 100; i++)
+	{
+		q.push(i);
+	}
 
-	// 최상위 원소 삭제
-	s.pop();
+	while (q.empty() == false)
+	{
+		int value = q.front();
+		q.pop();
+		cout << value << endl;
+	}
+
+	int size = q.size();
+
+	return;
 }	
